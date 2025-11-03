e4b37e5c28d425baab00986f32bd8fb01dc988d3
Elimurray <eli.murr04@gmail.com>
2025-11-03 18:03:16 +1300

    withdraw fix

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { profileAPI } from "../services/api";
+import { profileAPI, ladderAPI } from "../services/api";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
